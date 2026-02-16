1028f515f5cdb850f50ffc916508efba5af5f239
iraka5 <irakanomena@gmail.com>
2026-02-16 15:24:05 +0300

    login

---
+++ controllers/views/login.php
@@ -0,0 +1,10 @@
+<h1>Connexion</h1>
+<form method="post" action="/login">
+    <label>Email :</label>
+    <input type="email" name="email" required>
+    <br>
+    <label>Mot de passe :</label>
+    <input type="password" name="password" required>
+    <br>
+    <button type="submit">Se connecter</button>
+</form>
