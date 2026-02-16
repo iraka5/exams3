5d70b5914a3b7edd80722d68d36cc787fabd54a8
iraka5 <irakanomena@gmail.com>
2026-02-16 15:58:44 +0300

    changes

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
-require 'flight/Flight.php';
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+?>
+<?php
+require __DIR__ . '/vendor/autoload.php';
 
 require_once __DIR__ . "/controllers/BesoinController.php";
 require_once __DIR__ . "/controllers/DonController.php";
