cceff3977e3a05ea617c59e7f8ae7ac2ff77ad9d
Tiantsoa Melissa <ralaivaotiantsoamelissa@gmail.com>
2026-02-17 14:02:10 +0300

    i

--- index.php
+++ index.php
@@ -1,20 +1,22 @@
 <?php
-// index.php (propre)
-
-// Démarrer la session
+// index.php
 session_start();
-
-// Afficher les erreurs (debug)
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+
+// Servir les fichiers statiques du dossier public
+$publicPath = __DIR__ . '/public';
+if (preg_match('#^/public/(.+)$#', $_SERVER['REQUEST_URI'], $matches)) {
+    $file = realpath($publicPath . '/' . $matches[1]);
+    if ($file && strpos($file, $publicPath) === 0 && is_file($file)) {
+        $mime = mime_content_type($file);
+        header('Content-Type: ' . $mime);
+        readfile($file);
+        exit;
+    }
+}
 
 // Charger Composer (Flight, etc.)
 require __DIR__ . '/vendor/autoload.php';
-
-// Charger la config DB + fonctions
-require_once __DIR__ . '/config/config.php';
-
-// Charger les routes
 require_once __DIR__ . '/routes.php';
-          
+?>
